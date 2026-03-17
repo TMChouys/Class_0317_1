da9b84bef3ba97f7559c4923b876b20f4b5e68c5
TMChouys <chouys@gs.takming.edu.tw>
2026-03-17 14:38:56 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ConsoleApp1
+﻿namespace Class_0317_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Test joe, tom, mary;
+            joe = new Test();
+            mary = new Test();
+
+            joe.Mid = 80;
+            joe.Final = 90;
+
+            mary.Mid = 60;
+            mary.Final = 66;
+
+            Console.WriteLine("Joe "  + joe.GetAvg());
+            tom = joe;
+
+            Console.WriteLine("Tom " + tom.GetAvg());
+            joe.SetGrade(99, 100);
+            Console.WriteLine("Tom " + tom.GetAvg());
+
         }
     }
 }
